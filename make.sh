ad448b3cd913afecc3cb9a1fdd8da65dc2cb08c8
Jorge Kalmbach <kalmbach@gmail.com>
2009-06-03 09:50:08 -0300

    fix make.sh

--- make.sh
+++ make.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-gcc tabu.c tabu_player.c tbw_window.c tbw_controls.c tbw.h owl-audio-player.h -Wall -o tabu `taglib-config --cflags --libs` `pkg-config --cflags --libs gtk+-2.0 gstreamer-0.10` -ltag_c
+gcc tabu.c tabu_player.c tbw_window.c tbw_controls.c tbw.h -Wall -o tabu `taglib-config --cflags --libs` `pkg-config --cflags --libs gtk+-2.0 gstreamer-0.10` -ltag_c
 
