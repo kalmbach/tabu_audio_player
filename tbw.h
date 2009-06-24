314ee6dc399f60338fb5dd3a35920ae4b48e41ab
jorge <jorge@thaddeus.(none)>
2009-06-24 00:51:12 -0300

    Version 1.3, Resize Window Feature

--- tbw.h
+++ tbw.h
@@ -4,10 +4,10 @@
 #include <gtk/gtk.h>
 
 /* Functions of tabu.c */
-void add_item_to_playlist ( GtkListStore *store, gchar* filename, gchar *uri);
+void add_item_to_playlist ( GtkListStore *store, gchar* filename, gchar *uri );
 GtkListStore *get_tabu_playlist ( );
-void scroll_to_song (GtkTreeIter iter);
-void clear_tabu_playlist ();
+void scroll_to_song ( GtkTreeIter iter );
+void clear_tabu_playlist ( );
 
 /* Functions of tbw_window.c */
 void tbw_window_pack ( GtkWidget *widget, gboolean expand, gboolean fill, guint padding );
@@ -17,12 +17,15 @@ GtkWidget *tbw_window_new ( GtkWindowType type );
 GtkWidget *tbw_controls_new ( GtkListStore *widget );
 
 /* Functions of tabu_player.c */
-void tabu_player_quit ();
-void tabu_player_main ();
-void tabu_player_next ();
-void tabu_player_previous ();
-void tabu_player_play ();
-gboolean tabu_player_is_playing ();
+void tabu_player_quit ( );
+void tabu_player_main ( );
+void tabu_player_next ( );
+void tabu_player_previous ( );
+void tabu_player_play ( );
+void tabu_player_clear_playlist ( );
+void tabu_player_play_selection ( GtkTreeSelection *selection );
+void tabu_player_remove_selection ( GtkTreeSelection *selection );
+gboolean tabu_player_is_playing ( );
 
 #endif
 
