65b4dccad7f56d746abeed9a6457c55875d8ee29
Priyank Gosalia <priyankmg@gmail.com>
2009-06-26 21:14:04 +0530

    Add autogen.sh
    * configure.ac: somehow this file contained DOS style CR/LF. converted
      it back to unix format and some minor fixes

---
+++ autogen.sh
@@ -0,0 +1,31 @@
+#!/bin/sh -e
+
+cd `dirname $0`
+
+if [ "$1" == "--dist" ]; then
+	ver=`grep AC_INIT configure.ac|sed 's/.*, \([0-9\.]*\), .*/\1/'`
+	git archive --format=tar --prefix=tabu_audio_player-$ver/ HEAD | tar xf -
+	git log --no-merges |git name-rev --tags --stdin > tabu_audio_player-$ver/ChangeLog
+	cd tabu_audio_player-$ver
+	./autogen.sh --git
+	cd ..
+	tar czf tabu_audio_player-$ver.tar.gz tabu_audio_player-$ver
+	rm -rf tabu_audio_player-$ver
+	exit 0
+fi
+
+cat /usr/share/aclocal/libtool.m4 >> aclocal.m4
+
+intltoolize -c -f --automake
+libtoolize -f -c
+aclocal --force
+autoheader -f
+autoconf -f
+cp -f $(dirname $(which automake))/../share/automake/mkinstalldirs ./
+cp -f $(dirname $(which automake))/../share/gettext/config.rpath ./
+automake -a -c --gnu --foreign
+
+if [ "$1" == "--git" ]; then
+	rm -rf autom4te.cache
+fi
+
